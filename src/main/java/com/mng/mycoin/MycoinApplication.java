b93c6f412815c3ccf9efba788fdda426a022a131
kyun9 <gusl108@naver.com>
2022-06-01 14:35:01 +0900

    ADD 스프링부트 프레임등록

---
+++ src/main/java/com/mng/mycoin/MycoinApplication.java
@@ -0,0 +1,13 @@
+package com.mng.mycoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycoinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mng/mycoin/MycoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mng.mycoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
